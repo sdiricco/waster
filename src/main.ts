b67308fd34fca88b303b503cd74a4511f4b54657
simone <sdiricco.91@gmail.com>
2022-08-18 21:28:42 +0200

    add initial porting

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+
+/*********************************************
+ * IONIC
+ *********************************************/
+import { IonicVue } from '@ionic/vue';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+
+/*********************************************
+ * FONT AWESOME
+ *********************************************/
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+const app = createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(IonicVue)
+  .use(router);
+  
+router.isReady().then(() => {
+  app.mount('#app');
+});
