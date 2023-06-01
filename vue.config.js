7008ec6fa2b84192ba20da30cf8884d37736a671
Simone <sdiricco.91@gmail.com>
2023-06-01 16:45:40 +0200

    fix name app

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// vue.config.js in your project (IF NOT EXIST, create new one)
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+      name: 'Differenziata Porcari', // <---- this is PWA name
+    }
+  }
