90816e6c9e4f3d72579948dbd88e0abf1d9fd026
Simone <sdiricco.91@gmail.com>
2023-06-02 19:55:10 +0200

    add refactoring from bassano

--- src/views/OverviewPage.vue
+++ src/views/OverviewPage.vue
@@ -2,25 +2,24 @@
   <ion-page>
     <AppHeader title="Calendario" />
     <AppContent>
-      <div>
-        <MunicipalityCard municipality="Porcari" :next-date="state.date" />
-        <WasteCard v-if="state.wasteList && state.wasteList.length" :date="state.date" :wastes="state.wasteList" />
-        <NoWasteCard v-else />
-      </div>
+      <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+      <MunicipalityCard municipality="Porcari" :next-date="state.date" />
+      <WasteCard :date="state.date" :wastes="state.wasteList" />
     </AppContent>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-import { IonPage } from "@ionic/vue";
+import { IonPage, IonRefresher, IonRefresherContent } from "@ionic/vue";
 import { getNextDay } from "../services/date";
-import * as wastePorcari from "@/services/waste-porcari";
+import * as wasteBassanoZoneA from "@/services/waste-porcari";
 import { onMounted, reactive } from "vue";
 import AppHeader from "@/components/AppHeader.vue";
 import AppContent from "@/components/AppContent.vue";
 import MunicipalityCard from "@/components/MunicipalityCard.vue";
 import WasteCard from "@/components/WasteCard.vue";
-import NoWasteCard from "@/components/NoWasteCard.vue";
 
 /*********************************************************/
 /* INTERFACES */
@@ -36,10 +35,18 @@ let state = reactive<STATE>({
   date: new Date(),
   wasteList: [],
 });
+const handleRefresh = (event: CustomEvent) => {
+  setTimeout(() => {
+    // Any calls to load data go here
+    state.date = getNextDay();
+    state.wasteList = wasteBassanoZoneA.getWaste(state.date);
+    event.target.complete();
+  }, 1000);
+};
 
 onMounted(() => {
   state.date = getNextDay();
-  state.wasteList = wastePorcari.getWaste(state.date);
+  state.wasteList = wasteBassanoZoneA.getWaste(state.date);
 });
 </script>
 
